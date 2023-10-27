d81f610eaa2339cd8bc5f182a71de9a620016a7e
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2023-10-27 23:56:05 +0200

    Delete GoNetDev-master directory

--- GoNetDev-master/Protocols/Utils/Concat_slices.go
+++
@@ -1,9 +0,0 @@
-package Utils
-
-func ConcatAppend(slices [][]byte) []byte {
-	var tmp []byte
-	for _, s := range slices {
-		tmp = append(tmp, s...)
-	}
-	return tmp
-}
--- GoNetDev-master/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/sabouaram/GoNetDev
-
-go 1.15
-
-require github.com/prometheus/client_golang v1.9.0
